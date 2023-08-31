940339fb2bb767ac83b4b790d5373ee85345861d
wardfranssen <141460207+wardfranssen@users.noreply.github.com>
2023-08-31 17:58:03 +0200

    Update dailyword.py

--- dailyword.py
+++ dailyword.py
@@ -1,23 +1,27 @@
 import random
 import os
 
+# picks a random word from dailywords.txt
 with open("dailywords.txt") as wordlists:
     word_list = wordlists.read().splitlines()
     daily_word = random.choice(word_list)
+    word_list.close()
 
-with open("dailywords.txt", "r") as wordlistss:
-    content = wordlistss.read()
 
-with open("dailywords.txt", "w") as wordlistss:
-    wordlistss.write(content.replace(daily_word, ""))
+with open("dailywords.txt", "r") as dailywords:
+    content = dailywords.read()
+    dailywords.close()
 
-with open("Already_had_daily.txt", "a") as wordlist:
-    wordlist.write(daily_word + "\n")
-    wordlist.close()
+with open("dailywords.txt", "w") as dailywords:
+    dailywords.write(content.replace(daily_word, ""))
 
-open('.dailyword.txt', 'w').close()
-with open(".dailyword.txt", "a") as dailywordtxt:
-    dailywordtxt.write(daily_word)
-    dailywordtxt.close()
+with open("Already_had_daily.txt", "a") as Already_had:
+    Already_had.write(daily_word + "\n")
+    Already_had.close()
+
+open('dailyword.txt', 'w').close() # clears dailyword.txt
+with open("dailyword.txt", "a") as dailyword:
+    dailyword.write(daily_word)
+    dailyword.close()
 
 #os.system("shutdown /s /t 1")
