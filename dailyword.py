3a04d7c8459f6d0b0084ebfd506debd7d8df6492
wardfranssen <141460207+wardfranssen@users.noreply.github.com>
2023-08-31 18:04:52 +0200

    Update dailyword.py
    added comments so I understand the code better

--- dailyword.py
+++ dailyword.py
@@ -7,19 +7,22 @@ with open("dailywords.txt") as wordlists:
     daily_word = random.choice(word_list)
     word_list.close()
 
-
+# reads the dailywords so it can remove the new daily word
 with open("dailywords.txt", "r") as dailywords:
     content = dailywords.read()
     dailywords.close()
 
+# removes the daily word in the dailywords file
 with open("dailywords.txt", "w") as dailywords:
     dailywords.write(content.replace(daily_word, ""))
 
+# adds the new daily word to the file
 with open("Already_had_daily.txt", "a") as Already_had:
     Already_had.write(daily_word + "\n")
     Already_had.close()
 
 open('dailyword.txt', 'w').close() # clears dailyword.txt
+# writes the new daily word in the file
 with open("dailyword.txt", "a") as dailyword:
     dailyword.write(daily_word)
     dailyword.close()
